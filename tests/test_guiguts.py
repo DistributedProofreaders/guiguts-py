756f368c3237cfe0c56c9179a2a7dbea0f5e0524
Nigel <windymilla@users.noreply.github.com>
2023-12-05 19:58:37 +0000

    Check pytest error reported correctly

--- tests/test_guiguts.py
+++ tests/test_guiguts.py
@@ -28,7 +28,7 @@ def test_preferences():
     assert preferences.get("pkey1") == "pvalue1"
     assert preferences.get_default("pkey1") == "pdefault1"
     keys = preferences.keys()
-    assert len(keys) == 1
+    assert len(keys) == 2
     assert "pkey1" in keys
 
 
