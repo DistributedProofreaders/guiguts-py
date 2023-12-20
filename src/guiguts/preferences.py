cdce0113c2b81105f8845aae67b13dc5f5d43989
Nigel <windymilla@users.noreply.github.com>
2023-12-20 20:40:25 +0000

    Add optional callback to preferences
    All callback functions are called after prefs are loaded and the
    UI is ready. This gives a chance for a pref like "Auto Image" to
    set the UI correctly on startup.

--- src/guiguts/preferences.py
+++ src/guiguts/preferences.py
@@ -9,12 +9,35 @@ from guiguts.utilities import is_x11, _called_from_test
 class Preferences:
     """Handle setting/getting/saving/loading/defaulting preferences.
 
-    Load/Save preferences in temporary file when testing."""
+    Call `add` to create/define each preference, giving its default value,
+    and optionally a callback function, e.g. if loading a pref requires an
+    initial UI setting. Once UI is ready, call `run_callbacks` to deal with
+    all required side effects. Get/set value using preferences[key].
+
+    Load/Save preferences in temporary file when testing.
+
+    TODO: Currently 3 dictionaries: dict is the one loaded/save in JSON file.
+    Consider whether one dictionary with each item consisting of default, value
+    & callback would be better/safer.
+    Consider whether exception should be raised if unknown key is used, rather
+    than just returning None. Would probably need explicit call to load, rather
+    than loading on instantiation. Also, may be less forward/backward compatible
+    when new preferences are added in future releases.
+
+    Attributes:
+        dict: dictionary of values for prefs.
+        defaults: dictionary of values for defaults.
+        callbacks: dictionary of callbacks - typically a function to
+          deal with initial side effect of loading the pref, which will be
+          called after all prefs have been loaded and UI is ready. Not called
+          each time pref is changed.
+    """
 
     def __init__(self):
         """Initialize by loading from JSON file."""
         self.dict = {}
         self.defaults = {}
+        self.callbacks = {}
         if is_x11():
             self.prefsdir = os.path.join(os.path.expanduser("~"), ".ggpreferences")
         else:
@@ -43,7 +66,7 @@ class Preferences:
             Preferences value; default for ``key`` if no preference set;
             ``None`` if no default for ``key``.
         """
-        return self.dict.get(key, self.get_default(key))
+        return self.dict.get(key, self.defaults.get(key))
 
     def __setitem__(self, key, value):
         """Set preference value and save to file.
@@ -81,6 +104,17 @@ class Preferences:
         """
         return self.defaults.get(key)
 
+    def set_callback(self, key, callback):
+        """Add a callback for preference. Callback will be run when all
+          prefs are loaded and UI is ready.
+
+        Args:
+            key: Name of preference.
+            callback: Function to call if side effect required, e.g. to
+              update UI to reflect a setting.
+        """
+        self.callbacks[key] = callback
+
     def keys(self):
         """Return list of preferences keys.
 
@@ -100,6 +134,15 @@ class Preferences:
         if os.path.isfile(self.prefsfile):
             with open(self.prefsfile, "r") as fp:
                 self.dict = json.load(fp)
+
+    def run_callbacks(self):
+        """Run all defined callbacks, passing value as argument.
+
+        Should be called after prefs are loaded and UI is ready"""
+        for key in self.callbacks.keys():
+            callback = self.callbacks[key]
+            if callback:
+                callback(self.dict[key])
 
     def _remove_test_prefs_file(self):
         """Remove temporary JSON file used for prefs during testing."""
