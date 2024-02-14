3229e53c987cf2b4eae178bf1e2bd68037e0d65d
Nigel <windymilla@users.noreply.github.com>
2024-02-14 16:20:41 +0000

    Remove experimental "spawn process"
    Superseded by decision to implement tools as python modules.

--- src/guiguts/child.py
+++
@@ -1,8 +0,0 @@
-# Child process test file
-import sys
-
-input_string = input()
-print(f'Received "{input_string}"')
-output_string = input_string.upper()
-print(f'Output "{output_string}"')
-print("This should appear in stderr", file=sys.stderr)
