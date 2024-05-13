7316a38f8033532854c97817c73ff33e25077c61
Nigel <windymilla@users.noreply.github.com>
2024-05-13 20:30:04 +0100

    Add remaining unmatched text checks
    Block Markup, Brackets & Curly quotes

--- src/guiguts/application.py
+++ src/guiguts/application.py
@@ -26,7 +26,10 @@ from guiguts.misc_tools import (
     basic_fixup_check,
     page_separator_fixup,
     PageSepAutoType,
-    unmatched_markup_dp,
+    unmatched_dp_markup,
+    unmatched_brackets,
+    unmatched_curly_quotes,
+    unmatched_block_markup,
 )
 from guiguts.page_details import PageDetailsDialog
 from guiguts.preferences import preferences, PrefKey
@@ -451,6 +454,10 @@ Fifth Floor, Boston, MA 02110-1301 USA."""
         menu_tools.add_button("R~ewrap Selection", self.file.rewrap_selection)
         menu_tools.add_separator()
         menu_tools.add_button("~Unmatched DP Markup", unmatched_markup_dp)
+        menu_tools.add_button("Unmatched Bloc~k Markup", unmatched_block_markup)
+        menu_tools.add_button("Unmatched ~DP Markup", unmatched_dp_markup)
+        menu_tools.add_button("Unmatched ~Brackets", unmatched_brackets)
+        menu_tools.add_button("Unmatched Curly ~Quotes", unmatched_curly_quotes)
 
     def init_view_menu(self) -> None:
         """Create the View menu."""
