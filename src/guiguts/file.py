585e33d42147b6de5b2b1be4f9d30d3584cba548
Nigel <windymilla@users.noreply.github.com>
2024-09-29 15:32:26 +0100

    Add "Save a Copy As" menu option
    Saves a copy of the current file, but does not change the
    current filename, nor the "edited" flag.
    Name of saved copy does get inserted in the
    "Recent Documents" menu.

--- src/guiguts/file.py
+++ src/guiguts/file.py
@@ -290,12 +290,27 @@ class File:
             initialfile=os.path.basename(self.filename),
             initialdir=os.path.dirname(self.filename),
             filetypes=[("All files", "*")],
+            title="Save As",
         ):
             self.store_recent_file(fn)
             self.filename = fn
             self.save_file()
         grab_focus(root(), maintext())
         return fn
+
+    def save_copy_as_file(self) -> None:
+        """Save copy of current text as new file, without affecting
+        current filename or "modified" flag."""
+        if fn := filedialog.asksaveasfilename(
+            initialfile=os.path.basename(self.filename),
+            initialdir=os.path.dirname(self.filename),
+            filetypes=[("All files", "*")],
+            title="Save a Copy As",
+        ):
+            self.store_recent_file(fn)
+            maintext().do_save(fn, clear_modified_flag=False)
+            self.save_bin(fn)
+        grab_focus(root(), maintext())
 
     def check_save(self) -> bool:
         """If file has been edited, check if user wants to save,
@@ -419,6 +434,7 @@ class File:
         recents.insert(0, filename)
         del recents[NUM_RECENT_FILES:]
         preferences.set(PrefKey.RECENT_FILES, recents)
+        self._filename_callback()
 
     def remove_recent_file(self, filename: str) -> None:
         """Remove given filename from list of recent files.
@@ -430,6 +446,7 @@ class File:
         if filename in recents:
             recents.remove(filename)
             preferences.set(PrefKey.RECENT_FILES, recents)
+        self._filename_callback()
 
     def set_page_marks(self, page_details: PageDetails) -> None:
         """Set page marks from keys/values in dictionary.
