287d6e242668b4732463997ef09fd5bc6a95deba
Nigel <windymilla@users.noreply.github.com>
2025-01-28 19:51:01 +0000

    Add Auto-Illustrations feature (#706)
    Very similar to GG1 feature

--- src/guiguts/misc_dialogs.py
+++ src/guiguts/misc_dialogs.py
@@ -95,7 +95,7 @@ class PreferencesDialog(ToplevelDialog):
             font_frame,
             textvariable=PersistentString(PrefKey.TEXT_FONT_FAMILY),
             width=30,
-            validate="all",
+            validate=tk.ALL,
             validatecommand=(self.register(is_valid_font), "%P"),
             values=font_list,
             state="readonly",
