8aeeb02256c89f1d70bc814eeaf9f813d5168a20
Nigel <windymilla@users.noreply.github.com>
2025-06-03 20:45:40 +0100

    Try both

--- src/guiguts/root.py
+++ src/guiguts/root.py
@@ -10,7 +10,7 @@ from typing import Any
 
 try:  # Not yet supported for Tk9
     from tkinterdnd2 import TkinterDnD  # type: ignore[import-untyped]
-except (ImportError, tk.TclError):
+except (ImportError, RuntimeError, tk.TclError):
     TkinterDnD = tk
 
 from guiguts.preferences import preferences, PrefKey
