e89f5acc5a28b59e691f598e66161c3afe23c990
Nigel <windymilla@users.noreply.github.com>
2025-06-21 16:22:14 +0100

    Add View Options to Basic Fixup (#1141)
    1. Add View Options
    2. Make error type use the error prefix (default red text)
    3. Small code re-org to bring into line with more recent
    checkers

--- src/guiguts/misc_tools.py
+++ src/guiguts/misc_tools.py
@@ -95,6 +95,33 @@ def tool_save() -> bool:
     return save and bool(the_file().save_file())
 
 
+BASIC_FIXUP_CHECKER_FILTERS = [
+    CheckerFilterErrorPrefix("Multiple spaces: ", "Multiple spaces: "),
+    CheckerFilterErrorPrefix("Spaced hyphen: ", "Spaced hyphen: "),
+    CheckerFilterErrorPrefix("Spaced period: ", "Spaced period: "),
+    CheckerFilterErrorPrefix("Spaced exclamation mark: ", "Spaced exclamation mark: "),
+    CheckerFilterErrorPrefix("Spaced question mark: ", "Spaced question mark: "),
+    CheckerFilterErrorPrefix("Spaced comma: ", "Spaced comma: "),
+    CheckerFilterErrorPrefix("Spaced colon: ", "Spaced colon: "),
+    CheckerFilterErrorPrefix("Spaced semicolon: ", "Spaced semicolon: "),
+    CheckerFilterErrorPrefix("Spaced open quote: ", "Spaced open quote: "),
+    CheckerFilterErrorPrefix("Spaced close quote: ", "Spaced close quote: "),
+    CheckerFilterErrorPrefix("Spaced open bracket: ", "Spaced open bracket: "),
+    CheckerFilterErrorPrefix("Spaced close bracket: ", "Spaced close bracket: "),
+    CheckerFilterErrorPrefix("Trailing spaces: ", "Trailing spaces: "),
+    CheckerFilterErrorPrefix("Thought break: ", "Thought break: "),
+    CheckerFilterErrorPrefix("1/l scanno: ", "1/l scanno: "),
+    CheckerFilterErrorPrefix("Ellipsis spacing: ", "Ellipsis spacing: "),
+    CheckerFilterErrorPrefix("Spaced guillemet: ", "Spaced guillemet: "),
+]
+
+
+class BasicFixupCheckerViewOptionsDialog(CheckerViewOptionsDialog):
+    """Minimal class to identify dialog type."""
+
+    manual_page = "Tools_Menu#Basic_Fixup"
+
+
 class BasicFixupCheckerDialog(CheckerDialog):
     """Basic Fixup dialog."""
 
@@ -109,153 +136,142 @@ class BasicFixupCheckerDialog(CheckerDialog):
                     "Left click: Select & find issue",
                     "Right click: Hide issue",
                     f"With {cmd_ctrl_string()} key: Also fix issue",
-                    "With Shift key: Also hide/fix all matching issues",
+                    "With Shift key: Also hide/fix all exactly matching issues",
                 ]
             ),
             **kwargs,
         )
 
 
-def process_fixup(checker_entry: CheckerEntry) -> None:
-    """Process the fixup error."""
-    if checker_entry.text_range is None:
-        return
-    entry_type = checker_entry.text.split(":", 1)[0]
-    start_mark = BasicFixupCheckerDialog.mark_from_rowcol(
-        checker_entry.text_range.start
-    )
-    end_mark = BasicFixupCheckerDialog.mark_from_rowcol(checker_entry.text_range.end)
-    match_text = maintext().get(start_mark, end_mark)
-    # Several fixup errors are "Spaced ..." and need spaces removing.
-    if entry_type.startswith("Spaced "):
-        replacement_text = match_text.replace(" ", "")
-        maintext().replace(start_mark, end_mark, replacement_text)
-    elif entry_type == "Trailing spaces":
-        replacement_text = match_text.replace(" ", "")
-        maintext().replace(start_mark, end_mark, replacement_text)
-    elif entry_type == "Multiple spaces":
-        # Leave the non-space and first space alone
-        replacement_text = match_text[2:].replace(" ", "")
-        maintext().replace(f"{start_mark} + 2c", end_mark, replacement_text)
-    elif entry_type == "Thought break":
-        maintext().replace(start_mark, end_mark, "<tb>")
-    elif entry_type == "1/l scanno":
-        replacement_text = match_text.replace("llth", "11th").replace("lst", "1st")
-        maintext().replace(start_mark, end_mark, replacement_text)
-    elif entry_type == "Ellipsis spacing":
-        maintext().replace(start_mark, end_mark, " ...")
-    else:
-        return
+class BasicFixup:
+    """Run Basic Fixup check and allows user to fix errors."""
 
+    def __init__(self) -> None:
+        """Initialize HTML Validator."""
+        self.dialog = BasicFixupCheckerDialog.show_dialog(
+            rerun_command=self.run,
+            process_command=self.process_fixup,
+            view_options_dialog_class=BasicFixupCheckerViewOptionsDialog,
+            view_options_filters=BASIC_FIXUP_CHECKER_FILTERS,
+        )
 
-def sort_key_type(
-    entry: CheckerEntry,
-) -> tuple[int, str, int, int]:
-    """Sort key function to sort Fixup entries by text, putting identical upper
-        and lower case versions together.
+    def run(self) -> None:
+        """Check the currently loaded file for basic fixup errors."""
 
-    Differs from default alpha sort in using the text up to the colon
-    (the type of error) as the primary sort key. No need to deal with different
-    entry types for Fixup, and all entries have a text_range.
-    """
-    assert entry.text_range is not None
-    return (
-        entry.section,
-        entry.text.split(":")[0],
-        entry.text_range.start.row,
-        entry.text_range.start.col,
-    )
+        if not tool_save():
+            return
 
+        self.dialog.reset()
 
-def basic_fixup_check() -> None:
-    """Check the currently loaded file for basic fixup errors."""
+        # Check lines that aren't in block markup
+        in_block = in_poem = False
+        for line, line_num in maintext().get_lines():
+            # Ignore lines within block markup
+            if re.match(rf"/{BLOCK_TYPES}", line):
+                in_block = True
+            elif re.match(rf"{BLOCK_TYPES}/", line):
+                in_block = False
+            if in_block:
+                continue
 
-    if not tool_save():
-        return
+            # Ignore any poem line number (with preceding/trailing spaces)
+            if re.match(rf"/{POEM_TYPES}", line):
+                in_poem = True
+            elif re.match(rf"{POEM_TYPES}/", line):
+                in_poem = False
+            test_line = line
+            if in_poem and (match := re.search(r"\s{2,}\d+\s*$", line)):
+                test_line = line[: match.start(0)]
 
-    checker_dialog = BasicFixupCheckerDialog.show_dialog(
-        rerun_command=basic_fixup_check,
-        process_command=process_fixup,
-        sort_key_alpha=sort_key_type,
-    )
-    checker_dialog.reset()
+            # Dictionary of regexes:
+            # One or two capture groups in each regex, with the characters being checked
+            # captured by one of them - these will be highlighted in error message.
+            #
+            # Multiple spaces between non-space characters
+            # Space around single hyphen (not at start of line)
+            # Space before single period (not at start of line and not decimal point)
+            # Space before exclamation mark, question mark, comma, colon, or semicolon
+            # Space before close or after open quotes (where unambiguous)
+            # Space before close or after open brackets
+            # Trailing spaces
+            # Thought break (possibly poorly formed)
+            # 1/l scanno, e.g. llth
+            fixup_regexes = {
+                "Multiple spaces: ": r"(\S\s{2,})(?=\S)",  # Capture character before too
+                "Spaced hyphen: ": r"\S( +- *)(?!-)|(?<!-)( *- +)",
+                "Spaced period: ": r"\S( +\.)(?![\d\.])",
+                "Spaced exclamation mark: ": r"( +!)",
+                "Spaced question mark: ": r"( +\?)",
+                "Spaced comma: ": r"( +,)",
+                "Spaced colon: ": r"( +:)",
+                "Spaced semicolon: ": r"( +;)",
+                "Spaced open quote: ": r'(^" +|[‘“] +)',
+                "Spaced close quote: ": r'( +"$| +”)',
+                "Spaced open bracket: ": r"([[({] )",
+                "Spaced close bracket: ": r"( [])}])",
+                "Trailing spaces: ": r"(.? +$)",
+                "Thought break: ": r"^(\s*(\*\s*){4,})$",
+                "1/l scanno: ": r"(?<!\p{Letter})(lst|llth)\b",
+                "Ellipsis spacing: ": r"(?<=[^\.\!\? \"'‘“])(\.{3})(?![\.\!\?])",
+                "Spaced guillemet: ": r"(«\s+|\s+»)",
+            }
+            # Some languages use inward pointing guillemets - catch most of them.
+            # Do inward check if first language is one of these, or English with one of these.
+            inward_langs = r"(de|hr|cs|da|hu|pl|sr|sk|sl|sv)"
+            languages = maintext().get_language_list()
+            if re.match(inward_langs, languages[0]) or (
+                re.match("en", languages[0])
+                and any(re.match(inward_langs, lang) for lang in languages)
+            ):
+                fixup_regexes["Spaced guillemet: "] = r"(»\s+|\s+«)"
 
-    # Check lines that aren't in block markup
-    in_block = in_poem = False
-    for line, line_num in maintext().get_lines():
-        # Ignore lines within block markup
-        if re.match(rf"/{BLOCK_TYPES}", line):
-            in_block = True
-        elif re.match(rf"{BLOCK_TYPES}/", line):
-            in_block = False
-        if in_block:
-            continue
+            for prefix, regex in fixup_regexes.items():
+                for match in re.finditer(regex, test_line):
+                    group = 2 if match[1] is None else 1
+                    self.dialog.add_entry(
+                        line,
+                        IndexRange(
+                            IndexRowCol(line_num, match.start(group)),
+                            IndexRowCol(line_num, match.end(group)),
+                        ),
+                        match.start(group),
+                        match.end(group),
+                        error_prefix=prefix,
+                    )
+        self.dialog.display_entries()
 
-        # Ignore any poem line number (with preceding/trailing spaces)
-        if re.match(rf"/{POEM_TYPES}", line):
-            in_poem = True
-        elif re.match(rf"{POEM_TYPES}/", line):
-            in_poem = False
-        test_line = line
-        if in_poem and (match := re.search(r"\s{2,}\d+\s*$", line)):
-            test_line = line[: match.start(0)]
-
-        # Dictionary of regexes:
-        # One or two capture groups in each regex, with the characters being checked
-        # captured by one of them - these will be highlighted in error message.
-        #
-        # Multiple spaces between non-space characters
-        # Space around single hyphen (not at start of line)
-        # Space before single period (not at start of line and not decimal point)
-        # Space before exclamation mark, question mark, comma, colon, or semicolon
-        # Space before close or after open quotes (where unambiguous)
-        # Space before close or after open brackets
-        # Trailing spaces
-        # Thought break (possibly poorly formed)
-        # 1/l scanno, e.g. llth
-        fixup_regexes = {
-            "Multiple spaces: ": r"(\S\s{2,})(?=\S)",  # Capture character before too
-            "Spaced hyphen: ": r"\S( +- *)(?!-)|(?<!-)( *- +)",
-            "Spaced period: ": r"\S( +\.)(?![\d\.])",
-            "Spaced exclamation mark: ": r"( +!)",
-            "Spaced question mark: ": r"( +\?)",
-            "Spaced comma: ": r"( +,)",
-            "Spaced colon: ": r"( +:)",
-            "Spaced semicolon: ": r"( +;)",
-            "Spaced open quote: ": r'(^" +|[‘“] +)',
-            "Spaced close quote: ": r'( +"$| +”)',
-            "Spaced open bracket: ": r"([[({] )",
-            "Spaced close bracket: ": r"( [])}])",
-            "Trailing spaces: ": r"(.? +$)",
-            "Thought break: ": r"^(\s*(\*\s*){4,})$",
-            "1/l scanno: ": r"(?<!\p{Letter})(lst|llth)\b",
-            "Ellipsis spacing: ": r"(?<=[^\.\!\? \"'‘“])(\.{3})(?![\.\!\?])",
-            "Spaced guillemet: ": r"(«\s+|\s+»)",
-        }
-        # Some languages use inward pointing guillemets - catch most of them.
-        # Do inward check if first language is one of these, or English with one of these.
-        inward_langs = r"(de|hr|cs|da|hu|pl|sr|sk|sl|sv)"
-        languages = maintext().get_language_list()
-        if re.match(inward_langs, languages[0]) or (
-            re.match("en", languages[0])
-            and any(re.match(inward_langs, lang) for lang in languages)
-        ):
-            fixup_regexes["Spaced guillemet: "] = r"(»\s+|\s+«)"
-
-        for prefix, regex in fixup_regexes.items():
-            prefix_len = len(prefix)
-            for match in re.finditer(regex, test_line):
-                group = 2 if match[1] is None else 1
-                checker_dialog.add_entry(
-                    prefix + line,
-                    IndexRange(
-                        IndexRowCol(line_num, match.start(group)),
-                        IndexRowCol(line_num, match.end(group)),
-                    ),
-                    match.start(group) + prefix_len,
-                    match.end(group) + prefix_len,
-                )
-    checker_dialog.display_entries()
+    def process_fixup(self, checker_entry: CheckerEntry) -> None:
+        """Process the fixup error."""
+        if checker_entry.text_range is None:
+            return
+        entry_type = checker_entry.error_prefix.removesuffix(": ")
+        start_mark = BasicFixupCheckerDialog.mark_from_rowcol(
+            checker_entry.text_range.start
+        )
+        end_mark = BasicFixupCheckerDialog.mark_from_rowcol(
+            checker_entry.text_range.end
+        )
+        match_text = maintext().get(start_mark, end_mark)
+        # Several fixup errors are "Spaced ..." and need spaces removing.
+        if entry_type.startswith("Spaced "):
+            replacement_text = match_text.replace(" ", "")
+            maintext().replace(start_mark, end_mark, replacement_text)
+        elif entry_type == "Trailing spaces":
+            replacement_text = match_text.replace(" ", "")
+            maintext().replace(start_mark, end_mark, replacement_text)
+        elif entry_type == "Multiple spaces":
+            # Leave the non-space and first space alone
+            replacement_text = match_text[2:].replace(" ", "")
+            maintext().replace(f"{start_mark} + 2c", end_mark, replacement_text)
+        elif entry_type == "Thought break":
+            maintext().replace(start_mark, end_mark, "<tb>")
+        elif entry_type == "1/l scanno":
+            replacement_text = match_text.replace("llth", "11th").replace("lst", "1st")
+            maintext().replace(start_mark, end_mark, replacement_text)
+        elif entry_type == "Ellipsis spacing":
+            maintext().replace(start_mark, end_mark, " ...")
+        else:
+            return
 
 
 class PageSepAutoType(StrEnum):
