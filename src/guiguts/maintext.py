1a7be0ba3a8e1fdc0f3a7e6f1f535b7f75576a05
Nigel <windymilla@users.noreply.github.com>
2025-09-24 16:14:36 +0100

    Align column ruler better
    It wasn't taking into account the additional padding added
    to the text window during #1125

--- src/guiguts/maintext.py
+++ src/guiguts/maintext.py
@@ -110,7 +110,7 @@ class TextColumnNumbers(tk.Text):
             parent,
             font=self.font,
             height=1,
-            padx=3,
+            padx=text_widget["padx"] + 3,
             borderwidth=0,
             highlightthickness=0,
             takefocus=False,
