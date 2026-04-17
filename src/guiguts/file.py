DistributedProofreaders/guiguts-py

commit 70d5e00b50dfa52ed9e69b227afde2cbe525718a

Add `Convert LaTeX Page Separators` feature (#1815)
* Add `Convert LaTeX Page Separators` feature

Fixes #1813

Supports:
1. `\\DPPageSep{123.png}{89}`
2. `\\PageSep{89}`
3. `%% -----File: 123.png---Folio 89-------`

src/guiguts/file.py
@@ -82,7 +82,7 @@ PAGE_FLAG_ROMAN = "R"
 # Label is calculated from other info after file load.
 PAGE_FLAG_REGEX = rf"{PAGE_FLAG_START_E}{PAGE_FLAG_PREFIX}(.+?){PAGE_FLAG_SEP_E}(.+?){PAGE_FLAG_SEP_E}(.+?){PAGE_FLAG_END_E}"
 
-PAGE_SEPARATOR_REGEX = r"File:.+?([^/\\ ]+)\.(png|jpg)"
+PAGE_SEPARATOR_REGEX = r"^File:.+?([^/\\ ]+)\.(png|jpg)"
 
 BOOKMARK_BASE = "Bookmark"
 BOOKMARK_START = f"{BOOKMARK_BASE}Start"

src/guiguts/application.py
@@ -50,6 +50,7 @@ from guiguts.html_tools import (
     HTMLLinksDialog,
     latex_svg_convert,
     latex_undo_autogen,
+    latex_convert_pageseps,
 )
 from guiguts.illo_sn_fixup import illosn_check
 
@@ -966,6 +967,10 @@ class Guiguts:
     def init_file_latex_menu(self, parent: MenuMetadata) -> None:
         """Create the File->LaTeX menu."""
         project_menu = parent.add_submenu("LaTe~X")
+        project_menu.add_button(
+            "Convert LaTeX ~Page Separators",
+            latex_convert_pageseps,
+        )
         project_menu.add_button(
             "~Convert File (m2svg)",
             latex_svg_convert,

src/guiguts/html_tools.py
@@ -18,11 +18,18 @@ import zipfile
 from PIL import Image, ImageTk, UnidentifiedImageError
 import regex as re
 import requests
+import roman  # type: ignore[import-untyped]
 
 from guiguts.checkers import CheckerDialog, CheckerEntry, CheckerEntrySeverity
 from guiguts.file import the_file
 from guiguts.html_convert import make_anchor_id
 from guiguts.maintext import maintext, menubar_metadata, HighlightTag
+from guiguts.page_details import (
+    PageDetail,
+    PageDetails,
+    STYLE_ARABIC,
+    STYLE_ROMAN,
+)
 from guiguts.preferences import (
     PersistentString,
     PersistentBoolean,
@@ -2557,5 +2564,73 @@ def latex_undo_autogen() -> None:
 
         # Start next search at the end of the previous markup
         start = maintext().index(endmark)
+
+    Busy.unbusy()
+
+
+def latex_convert_pageseps() -> None:
+    """Convert LaTeX page seps `\\PageSep{nnn}` to GG page seps."""
+    Busy.busy()
+    maintext().undo_block_begin()
+
+    page_details = PageDetails()
+    start_idx = "1.0"
+    png_num = 0
+    while True:
+        start_idx = maintext().search(
+            r"^\\(DP)?PageSep\{|^%+ *---+File:", start_idx, tk.END, regexp=True
+        )
+        if not start_idx:
+            break
+        latex_sep = maintext().get(start_idx, f"{start_idx} lineend")
+        # Try to deal with the most common LaTeX page sep formats
+        # \\DPPageSep{123.png}{89}
+        if match := re.match(
+            r"\\DPPageSep\{(.+?)\.png\}\{(.+?)\}", latex_sep, flags=re.IGNORECASE
+        ):
+            png_str = str(match.group(1))
+            folio_str = str(match.group(2))
+        # \\PageSep{89}
+        elif match := re.match(r"\\PageSep\{(.+?)\}", latex_sep, flags=re.IGNORECASE):
+            png_num += 1  # No PNG given, just use next in sequence
+            png_str = str(png_num)
+            folio_str = str(match.group(1))
+        # %% -----File: 123.png---Folio 89-------
+        elif match := re.match(
+            r"%+ *---+File: *(.+?)\.png[- ]+Folio +([^ -]+)",
+            latex_sep,
+            flags=re.IGNORECASE,
+        ):
+            png_str = str(match.group(1))
+            folio_str = str(match.group(2))
+        # Can't make sense of it - ignore
+        else:
+            continue
+        if len(png_str) < 4:
+            png_str = "0" * (4 - len(png_str)) + png_str
+        # Cope with Roman and Arabic folio numbers
+        try:
+            num = roman.fromRoman(folio_str.upper())
+            style = STYLE_ROMAN
+        except roman.InvalidRomanNumeralError:
+            try:
+                num = int(folio_str)
+                style = STYLE_ARABIC
+            except ValueError:
+                continue
+        page_details[png_str] = PageDetail(start_idx, style, str(num))
+        # Convert to a standard page sep line
+        standard_line = f"-----File: {png_str}.png"
+        standard_line += "-" * (75 - len(standard_line))
+        maintext().replace(start_idx, f"{start_idx} lineend", standard_line)
+        # Search from next line
+        start_idx = f"{start_idx} lineend"
+
+    # If any LaTeX page seps found, calculate the label strings, store in File class,
+    # and set up page marks at each position.
+    if page_details:
+        page_details.recalculate()
+        the_file().page_details = page_details
+        the_file().set_page_marks(page_details)
 
     Busy.unbusy()

commit 8b0a799a42752c5d93dbd4fb67c7295812192a5c

Fix bug with page separator recognition
Introduced by #1815

src/guiguts/file.py
@@ -82,7 +82,7 @@ PAGE_FLAG_ROMAN = "R"
 # Label is calculated from other info after file load.
 PAGE_FLAG_REGEX = rf"{PAGE_FLAG_START_E}{PAGE_FLAG_PREFIX}(.+?){PAGE_FLAG_SEP_E}(.+?){PAGE_FLAG_SEP_E}(.+?){PAGE_FLAG_END_E}"
 
-PAGE_SEPARATOR_REGEX = r"^File:.+?([^/\\ ]+)\.(png|jpg)"
+PAGE_SEPARATOR_REGEX = r"File:.+?([^/\\ ]+)\.(png|jpg)"
 
 BOOKMARK_BASE = "Bookmark"
 BOOKMARK_START = f"{BOOKMARK_BASE}Start"