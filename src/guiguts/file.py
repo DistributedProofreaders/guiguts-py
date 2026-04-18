7f4e05586802d2c9921d25f50a15af84995bc91e
Nigel <windymilla@users.noreply.github.com>
2026-04-18 15:50:26 +0100

    Fix bug again
    Remove code that tries to standardize page sep lines. It's not
    worth the trouble, and I don't think anything relies on it.

--- src/guiguts/file.py
+++ src/guiguts/file.py
@@ -669,8 +669,7 @@ class File:
         self.mark_page_boundaries()
 
     def mark_page_boundaries(self) -> None:
-        """Loop through whole file, ensuring all page separator lines
-        are in standard format, and setting page marks at the
+        """Loop through whole file setting page marks at the
         start of each page separator line.
         """
         if not self.contains_page_marks():
@@ -688,13 +687,7 @@ class File:
             line = maintext().get(line_start, line_end)
             # Always matches since same regex as earlier search
             if match := pattern.search(line):
-                (page, ext) = match.group(1, 2)
-                standard_line = f"-----File: {page}.{ext}"
-                standard_line += "-" * (75 - len(standard_line))
-                # Don't standarize line if it has a page marker flag on it, or you'll delete the flag!
-                if line != standard_line and not re.search(PAGE_FLAG_REGEX, line):
-                    maintext().delete(line_start, line_end)
-                    maintext().insert(line_start, standard_line)
+                page = match.group(1)
                 page_mark = page_mark_from_img(page)
                 maintext().set_mark_position(page_mark, IndexRowCol(line_start))
                 try:
