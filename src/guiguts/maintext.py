2fdf197b868a340edf15547c0df2c5ace57a531e
Nigel <windymilla@users.noreply.github.com>
2026-08-13 09:31:06 +0100

    Set Tk selection anchor when using Home/End keys (#1923)
    * Set Tk selection anchor when using Home/End keys
    
    When used without Shift, Home/End (Cmd-Left/Right on
    Macs) should set the selection anchor so that future
    adjustment of the selection works more predictably.
    
    Fixes #1920
    
    * Also fix Ctrl-a/e on Macs
    
    * Another small fix

--- src/guiguts/maintext.py
+++ src/guiguts/maintext.py
@@ -951,14 +951,7 @@ class MainText(tk.Text):
         self.bind_event(
             "<Escape>", lambda _: self.cancel_drag_sel(), add=True, bind_peer=True
         )
-
-        def set_anchor(_: tk.Event) -> str:
-            """Set anchor point at current position, then allow Shift-click to
-            execute its default behavior."""
-            self.mark_set(TK_ANCHOR_MARK, f"{self.index(tk.INSERT)}")
-            return ""
-
-        self.bind("<Shift-Button-1>", set_anchor)
+        self.bind("<Shift-Button-1>", lambda _event: self.set_tk_selection_anchor())
 
         # Override default left/right/up/down arrow key behavior if there is a selection
         # Above behavior would affect Shift-Left/Right/Up/Down, so also bind those to
@@ -2687,7 +2680,14 @@ class MainText(tk.Text):
         """
         sel_ranges = self.selected_ranges()
         if not sel_ranges:
-            return ""
+            if force_line:
+                sel_ranges.append(
+                    IndexRange(
+                        maintext().get_insert_index(), maintext().get_insert_index()
+                    )
+                )
+            else:
+                return ""
         pos = sel_ranges[-1].end if end else sel_ranges[0].start
         idx = pos.index()
         if force_line:
@@ -2696,6 +2696,7 @@ class MainText(tk.Text):
         self.focus_widget().mark_set(tk.INSERT, idx)
         self.focus_widget().see(tk.INSERT)
         self.clear_selection()
+        self.set_tk_selection_anchor()
         return "break"
 
     def move_to_start(self) -> None:
@@ -3920,6 +3921,11 @@ class MainText(tk.Text):
         while start := self.search(" +$", start, regexp=True):
             self.delete(start, f"{start} lineend")
 
+    def set_tk_selection_anchor(self) -> str:
+        """Set anchor point at current position."""
+        self.mark_set(TK_ANCHOR_MARK, f"{self.index(tk.INSERT)}")
+        return ""
+
     def go_home(self, event: tk.Event) -> str:
         """Handle the Home key. Either move to start of line, or if
         already there, move to first non-space on line."""
@@ -3955,6 +3961,7 @@ class MainText(tk.Text):
         # If no Shift, just moving, so clear selection
         else:
             self.clear_selection()
+            self.set_tk_selection_anchor()
         self.see(tk.INSERT)
         return "break"
 
@@ -3986,6 +3993,7 @@ class MainText(tk.Text):
         # If no Shift, just moving, so clear selection
         else:
             self.clear_selection()
+            self.set_tk_selection_anchor()
         self.see(tk.INSERT)
         return "break"
 
